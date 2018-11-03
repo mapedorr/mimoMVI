dab8461a94ca8ef09aef12e4f27c2d07288aede3
Carenalga <mapedorr@gmail.com>
2018-11-03 02:58:45 -0500

    Added information to facts and preview of its material

--- js/main.js
+++ js/main.js
@@ -7,21 +7,57 @@
 /**
  * C O N S T A N T S ──────────────────────────────────────────────────────────
  */
-const candidateA = 'Duque';
-const candidateB = 'Petro';
+const candidateA = 'Fulano';
+const candidateB = 'Sutano';
 const MAX_DAYS = 8;
+const INTENTIONS = {
+  p: 'Persuadir',
+  m: 'Motivar',
+  c: 'Engañar',
+  a: 'Enojar'
+};
+const GOAL_TYPE = {
+  p: 'p',
+  m: 'm',
+  c: 'c',
+  a: 'a'
+};
 const MATERIAL = {
+  // listados de adj. y sus. https://bit.ly/2JB6mSP 
   0: { desc: 'adulto contento', p: 2, m: 3, c: 1, a: 0 },
   1: { desc: 'adulto indigando', p: 1, m: 0, c: 3, a: 3 },
   2: { desc: 'casas bonitas', p: 2, m: 3, c: 2, a: 0 },
-  3: { desc: 'casas descuidadas', p: 1, m: 0, c: 2, a: 2 }
+  3: { desc: 'casas descuidadas', p: 1, m: 0, c: 2, a: 2 },
+  4: { desc: 'jugadas agresivas', p: 0, m: 3, c: 0, a: 3 },
+  5: { desc: 'jugadas impresionantes', p: 1, m: 3, c: 0, a: 0 }
 };
+
 const FACTS = [
   // d1
   [
-    { what: 'No subiré impuestos', who: candidateA, material: [0, 1, 2, 3] },
-    { what: 'No subiré impuestos', who: candidateB, material: [0, 1, 2, 3] }
+    {
+      who: candidateA,
+      where: 'En debate',
+      what: 'Hablando de propuestas para Economía',
+      goal: GOAL_TYPE.c,
+      material: [0, 1, 2, 3]
+    },
+    {
+      who: candidateB,
+      where: 'En debate',
+      what: 'Hablando de propuestas para Economía',
+      goal: GOAL_TYPE.a,
+      material: [0, 1, 2, 3]
+    },
+    {
+      who: 'Ecolín',
+      where: 'En partido contra Brumas',
+      what: 'Gana 2 a 0',
+      material: [4, 5]
+    }
   ]
+  // d2
+  
 ];
 const POPULATION = 20000;
 const MTL_AMPLITUDE_LEVEL = 100 / 3;
@@ -45,24 +81,25 @@ let debugShowSum = false;
 /**
  * U I   E L E M E N T S ──────────────────────────────────────────────────────
  */
-// module: facts
+// - - - module: facts
 let dayCount = null;
 let factsCount = null;
 let factDesc = null;
 let nextFactButton = null;
-// module: material
+let conMaterialDispenser = null;
+// - - - module: material
 let modMaterial = null;
 let materialSelects = null;
 let materialSentibars = null;
-// module: send
+// - - - module: send
 let modSendSentibar = null;
 let recButton = null;
 let modSumSentibar = null;
 let conDebugSum = null;
-// module: results
+// - - - module: results
 let endDayButton = null;
 let votingGroups = null;
-// debug
+// - - - debug
 let showNumbersCheck = null;
 let showSumCheck = null;
 
@@ -75,13 +112,19 @@ function turnOnMimo() {
   dayCount = $('#day-count');
   factsCount = $('#facts-count');
   nextFactButton = $('#next-fact');
+  conMaterialDispenser = $('#con-material-dispenser');
   factDesc = {
     who: $('#fact-desc #who'),
-    what: $('#fact-desc #what')
+    where: $('#fact-desc #where'),
+    what: $('#fact-desc #what'),
+    goal: $('#fact-desc #goal')
   };
 
   factDesc.who.hide();
+  factDesc.where.hide();
   factDesc.what.hide();
+  factDesc.goal.hide();
+  conMaterialDispenser.hide();
 
   // module: material
   modMaterial = $('.mod-mtl');
--- js/modFacts.js
+++ js/modFacts.js
@@ -21,6 +21,9 @@ function setDay() {
  * based on that.
  */
 function showCurrentNew() {
+  // remove all the material from the previous fact
+  conMaterialDispenser.find('.material-slot').remove();
+
   // pick the current fact and fill its material
   currentFact = dayFacts[currentFactIndex++];
   factsCount.text(`${currentFactIndex} / ${dayFacts.length}`);
@@ -28,9 +31,55 @@ function showCurrentNew() {
   // fill and show the description of the fact
   factDesc.who.find('span').text(currentFact.who);
   factDesc.who.show();
+  factDesc.where.find('span').text(currentFact.where);
+  factDesc.where.show();
   factDesc.what.find('span').text(currentFact.what);
   factDesc.what.show();
-  
+  factDesc.goal.find('span').text((currentFact.goal) ?
+    INTENTIONS[currentFact.goal]
+    : 'Informar');
+  factDesc.goal.show();
+
+  // show the material available for the fact
+  const dispenserBody = conMaterialDispenser.children('.body');
+  currentFact.material.forEach((value, index, array) => {
+    const createdElement = $(`
+      <div class="material-slot">
+        <p class="material-desc">${MATERIAL[value].desc}</p>
+        <div class="sentibar">
+          <div class="persuade cell">
+            <div class="fill"></div>
+            <div class="value">${MATERIAL[value].p}</div>
+          </div>
+          <div class="motivate cell">
+            <div class="fill"></div>
+            <div class="value">${MATERIAL[value].m}</div>
+          </div>
+          <div class="cajole cell">
+            <div class="fill"></div>
+            <div class="value">${MATERIAL[value].c}</div>
+          </div>
+          <div class="anger cell">
+            <div class="fill"></div>
+            <div class="value">${MATERIAL[value].a}</div>
+          </div>
+        </div>
+      </div>
+    `);
+
+    // update the sentibars for each created material
+    setSentibarValues({
+      sentibar: createdElement.find('.sentibar'),
+      material: MATERIAL[value]
+    });
+
+    dispenserBody.append(createdElement);
+  });
+
+  // show the material dispenser
+  conMaterialDispenser.show();
+  updateDebug();
+
   // fill the material selectors
   fillMaterials();
 
